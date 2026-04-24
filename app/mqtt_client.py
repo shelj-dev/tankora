651dba999118d85f20c9138ba4f81d26ce79c983
ShelJi <shelj.dev@gmail.com>
2026-04-24 12:40:38 +0530

    mqtt testing settedup

--- app/mqtt_client.py
+++ app/mqtt_client.py
@@ -8,13 +8,15 @@ import time
 
 from app.services import get_daily_gas_usage, predict_gas_last_days, send_alert_email, send_email
 from app.models import GasDevice, TelemetryLog, LeakageAlert
+from main.settings import GAS_THRESHOLD_PERCENTAGE
 
 
 logger = logging.getLogger(__name__)
 
 MQTT_BROKER = "127.0.0.1"
 MQTT_PORT = 1883
-MQTT_TOPIC = "tankora/gas_monitor"
+MQTT_TOPIC = "tankora/gas_monitor/data"
+MQTT_SUB_TOPIC = "tankora/gas_monitor/control"
 
 client = None
 mqtt_thread_started = False
@@ -25,6 +27,7 @@ def on_connect(client, userdata, flags, rc):
     if rc == 0:
         logger.info("MQTT Connected")
         client.subscribe(MQTT_TOPIC)
+        client.subscribe(MQTT_SUB_TOPIC)
     else:
         logger.error(f"MQTT Connection failed: {rc}")
 
@@ -40,54 +43,19 @@ def on_message(client, userdata, msg):
     try:
         data = json.loads(msg.payload.decode())
 
-        raw = data.get("g", 0)
-        leak = data.get("l", False)
+        gas = data.get("g", 0)
         weight = data.get("w", 0)
 
-        percent = min((raw / 65535.0) * 100, 100.0)
+        percent = round(min((gas / 65535.0) * 100, 100.0))
         
-        print(percent, leak, weight)
+        print(percent, weight)
 
-        process_sensor_data(percent, leak, weight)
+        process_sensor_data(percent, weight)
 
     except Exception as e:
         logger.exception(f"MQTT message error: {e}")
 
 
-# ---------------- BUSINESS LOGIC ---------------- #
-def process_sensor_data(percent, leak, weight):
-
-    device, _ = GasDevice.objects.get_or_create(
-        device_id="pico_gas_monitor"
-    )
-
-    device.current_level = percent
-    device.current_weight = weight
-    device.last_seen = timezone.now()
-
-    device.prediction = get_daily_gas_usage(device)
-    
-    device.save(update_fields=["current_level", "last_seen", "current_weight", "prediction"])
-
-    TelemetryLog.objects.create(
-        device=device,
-        level=percent,
-        current_weight=weight
-    )
-
-    # Prevent duplicate alerts
-    if leak:
-        if not LeakageAlert.objects.filter(device=device, resolved=False).exists():
-            LeakageAlert.objects.create(
-                device=device,
-                severity="HIGH"
-            )
-            device.valve_status = "CLOSED"
-            device.save()
-            if device.supplier_email:
-                send_alert_email()
-
-    logger.info(f"Data saved: {percent:.2f}% | Leak: {leak}")
 
 
 # ---------------- MQTT START ---------------- #
@@ -126,7 +94,7 @@ def start_mqtt():
     logger.info("MQTT background thread started")
 
 
-def mqtt_toggle_valve():
+def mqtt_send_value():
     global client
 
     if client is None:
@@ -138,21 +106,57 @@ def mqtt_toggle_valve():
             device_id="pico_gas_monitor"
         )
 
-        if device.valve_status == "CLOSED":
-            command = "OPEN_VALVE"
-        else:
-            command = "CLOSED_VALVE"
-
         payload = json.dumps({
-            "command": command,
-            "buzzer": "OFF"
+            "r": True,
+            "b": True,
+            "s": 180
         })
 
-        topic = f"tankora/{device.device_id}/command"
 
-        client.publish(topic, payload)
+        client.publish(MQTT_SUB_TOPIC, payload)
 
-        print(f"MQTT sent: {payload} → {topic}")
+        print(f"MQTT sent: {payload} → {MQTT_TOPIC}")
 
     except Exception as e:
         print("MQTT publish error:", e)
+
+
+
+
+
+# ---------------- BUSINESS LOGIC ---------------- #
+def process_sensor_data(percent, weight):
+
+    device, _ = GasDevice.objects.get_or_create(
+        device_id="pico_gas_monitor"
+    )
+
+    device.current_level = percent
+    device.current_weight = weight
+    device.last_seen = timezone.now()
+
+    device.prediction = get_daily_gas_usage(device)
+    
+    device.save(update_fields=["current_level", "last_seen", "current_weight", "prediction"])
+
+    TelemetryLog.objects.create(
+        device=device,
+        level=percent,
+        current_weight=weight
+    )
+    
+    leak = True if percent < GAS_THRESHOLD_PERCENTAGE else False
+
+    # Prevent duplicate alerts
+    if leak:
+        if not LeakageAlert.objects.filter(device=device, resolved=False).exists():
+            LeakageAlert.objects.create(
+                device=device,
+                severity="HIGH"
+            )
+            device.valve_status = "CLOSED"
+            device.save()
+            if device.supplier_email:
+                send_alert_email()
+
+    logger.info(f"Data saved: {percent:.2f}% | Leak: {leak}")
